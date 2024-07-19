5ffddecd80d537b9f673eed604d7768f531fe36b
Anabel Benítez <anabelbenitezglez@gmail.com>
2024-07-19 10:06:52 -0400

    Fix program

---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using BaseVisitor.Interfaces;
+using Example.VisitorImplementations;
+using Visitor.ASTNodes;
+
+IExpression expression = new MultiplicationExpression(
+    new AdditionExpression(
+        new NumberExpression(5),
+        new NumberExpression(3)
+    ),
+    new NumberExpression(2)
+);
+
+var formatVisitor = new FormatVisitor();
+var formatResult = formatVisitor.Visit(expression);
+Console.WriteLine(formatResult);
+
+Console.WriteLine("=======================================================");
+
+var evaluationVisitor = new EvaluationVisitor();
+var evaluationResult = evaluationVisitor.Visit(expression);
+Console.WriteLine($"Resultado: {evaluationResult}");
